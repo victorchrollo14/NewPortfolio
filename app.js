7f678f0a6c7e7504522196f8c300cd348dbe4101
Victor Immanuel <chrollolucilfer1402@gmail.com>
2023-02-16 08:01:41 +0530

    added js to hide and show nav on mobile

---
+++ app.js
@@ -0,0 +1,12 @@
+const navMenu = document.querySelector(".hamburger-menu");
+const sidePanel = document.querySelector(".side-panel");
+const cross = document.querySelector(".cross");
+
+// display and hide navigation
+navMenu.addEventListener("click", function(e){
+    sidePanel.style.display = "block";
+});
+
+cross.addEventListener("click", function(e){
+    sidePanel.style.display = "none";
+})

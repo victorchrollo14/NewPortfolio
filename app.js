64cc1b08ae81fd6422059a04e539b26a08134094
Victor Immanuel <chrollolucilfer1402@gmail.com>
2023-02-17 13:40:01 +0530

    added the image for about me section

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const navMenu = document.querySelector(".hamburger-menu");
 const sidePanel = document.querySelector(".side-panel");
 const cross = document.querySelector(".cross");
+const image = document.querySelector(".image");
+const frame = document.querySelector(".frame");
 
 // display and hide navigation
 navMenu.addEventListener("click", function(e){
@@ -10,3 +12,18 @@ navMenu.addEventListener("click", function(e){
 cross.addEventListener("click", function(e){
     sidePanel.style.display = "none";
 })
+
+// Image hover frame animation
+
+image.addEventListener("mouseover", function(e){
+    frame.classList.add("frame-up");
+
+})
+
+
+image.addEventListener("mouseout", function(e){
+    frame.classList.remove("frame-up");
+
+})
+
+

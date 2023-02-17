dfd973b3224745369be9404dab5d447ad1e6b508
Victor Immanuel <chrollolucilfer1402@gmail.com>
2023-02-17 13:56:18 +0530

    made some changes with the animation frame part

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const navMenu = document.querySelector(".hamburger-menu");
 const sidePanel = document.querySelector(".side-panel");
 const cross = document.querySelector(".cross");
-const image = document.querySelector(".image");
-const frame = document.querySelector(".frame");
 
 // display and hide navigation
 navMenu.addEventListener("click", function(e){
@@ -13,17 +11,5 @@ cross.addEventListener("click", function(e){
     sidePanel.style.display = "none";
 })
 
-// Image hover frame animation
-
-image.addEventListener("mouseover", function(e){
-    frame.classList.add("frame-up");
-
-})
-
-
-image.addEventListener("mouseout", function(e){
-    frame.classList.remove("frame-up");
-
-})
 
 

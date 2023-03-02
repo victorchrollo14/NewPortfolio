a031cac7480f7e8ccdb9047012b2b1ccbf22efa1
Victor Immanuel <chrollolucilfer1402@gmail.com>
2023-03-02 15:35:32 +0530

    add contact me section and footer

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const navMenu = document.querySelector(".hamburger-menu");
 const sidePanel = document.querySelector(".side-panel");
 const cross = document.querySelector(".cross");
+const mainBody = document.querySelector('.Victor');
+const navLinks = document.querySelector('.nav-links').children;
+console.log(navLinks);
 
 // display and hide navigation
 navMenu.addEventListener("click", function(e){
     sidePanel.style.display = "block";
+    mainBody.style.opacity = "0.5";
+    document.body.style.height = "100%";
+    document.body.style.overflowY = "hidden";
 });
 
-cross.addEventListener("click", function(e){
+cross.addEventListener("click", hideSidePanel);
+
+
+function hideSidePanel(){
     sidePanel.style.display = "none";
-})
+    document.body.style = 'initial';
+    mainBody.style = "initial";
+}
 
 
 
